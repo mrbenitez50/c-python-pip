cb423e615e5662392039e7561cb620e1cf6f102a
Martin <mrbenitez50@gmail.com>
2024-11-12 19:42:33 -0500

    guardando app y charts

---
+++ charts/charts.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def genrate_pie ():
+    labels = ['A', 'B', 'C']
+    values = [200, 34, 120]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('pie.png')
+    plt.close()
+
+
+
---
+++ charts/main.py
@@ -0,0 +1,9 @@
+import charts
+
+def run():
+    charts.genrate_pie()
+
+if __name__== '__main__':
+    run()
+
+    
